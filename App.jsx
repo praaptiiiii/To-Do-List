d1cae808e74a89218fd9b416ca6dcf0d7e106baa
Prapti <121173640+praaptiiiii@users.noreply.github.com>
2024-06-29 21:02:29 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function App() {
+  const [item, setItem] = React.useState("");
+  const [list, createList] = React.useState([]);
+
+  function handleChange(event) {
+    const newItem = event.target.value;
+    setItem(newItem);
+  }
+
+  function addItem(event) {
+    createList((prevItems) => {
+      return [...prevItems, item];
+    });
+    setItem("");
+  }
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={item} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {list.map((listItem) => (
+            <li>{listItem}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
