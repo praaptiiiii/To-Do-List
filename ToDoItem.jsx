d7996a7724920af33d48a8a5efae013e46680533
Prapti <121173640+praaptiiiii@users.noreply.github.com>
2024-06-29 23:03:40 +0530

    Update ToDoItem.jsx

--- ToDoItem.jsx
+++ ToDoItem.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 function ToDoItem(props) {
-  return <li>{props.text}</li>;
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
 }
 export default ToDoItem;
